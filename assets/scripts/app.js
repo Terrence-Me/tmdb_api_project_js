f527a2ffcf6122453211cf943c1e2677997a6683
Terrence Meikle <terrence_meikle@yahoo.com>
2023-01-07 19:45:26 -0500

    add misc edits

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,6 +1,7 @@
 const media = document.getElementById('media');
 
 const posterBaseUrl = 'https://www.themoviedb.org/t/p/w220_and_h330_face';
+const movieIdBaseUrl = 'https://www.themoviedb.org/movie/';
 
 const popularUrl =
   'https://api.themoviedb.org/3/movie/popular?api_key=6e33035fa0621772e26e7510c45c539e&language=en-US&page=1';
@@ -8,22 +9,22 @@ const popularUrl =
 const fetchPopular = async () => {
   let response = await fetch(popularUrl);
   let data = await response.json();
-  console.log(typeof data.results);
+  console.log(data.results);
   buildPopularCards(data);
-  //   return data.results;
 };
 
 const buildPopularCards = (data) => {
   console.log(data);
   media.innerHTML = data.results
     .map((movie, index) => {
-      //   console.log(posterBaseUrl`${movie.poster_path}`);
       return `<div class="column_content">
     <div class="card">
         <div class="card-image">
             <div class="card-wrapper">
-                <a class="image" href="#">
-                    <img src=${posterBaseUrl}${movie.poster_path} alt=${movie.title}>
+                <a class="image" href=${movieIdBaseUrl}${movie.id}>
+                    <img src=${posterBaseUrl}${movie.poster_path} alt=${
+        movie.title
+      }>
                 </a>
             </div>
             <div class="card-options">
@@ -38,7 +39,7 @@ const buildPopularCards = (data) => {
                     <div class="user-score">
                         <div class="percent">
                             <span class="icon">
-
+                                ${movie.vote_average * 10}%
                             </span>
                         </div>
                     </div>
